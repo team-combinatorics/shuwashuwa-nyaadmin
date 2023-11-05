e564eaf022278b6db5669bc317291bb49ab8cbbe
12f23eddde <12f23eddde@gmail.com>
2023-11-05 11:02:09 +0800

    Don't show sizepicker on mobile

--- src/composables/table-pagination.ts
+++ src/composables/table-pagination.ts
@@ -1,14 +1,39 @@
 // use a global pagination property to be shared between all tables
-export const tablePagination = reactive({
+
+import { useWindowSize } from '@vueuse/core';
+
+const windowSize = useWindowSize()
+const useMobileLayout = computed(() => {
+    return windowSize.width.value <= 480;
+})
+
+const mobilePagination = reactive({
+    page: 1,
+    pageSize: 40,
+    showSizePicker: false,
+    onChange: (page: number) => {
+        mobilePagination.page = page
+    },
+    onUpdatePageSize: (pageSize: number) => {
+        mobilePagination.pageSize = pageSize
+        mobilePagination.page = 1
+    }
+})
+
+const desktopPagination = reactive({
     page: 1,
     pageSize: 10,
     showSizePicker: true,
     pageSizes: [10, 20, 40],
     onChange: (page: number) => {
-        tablePagination.page = page
+        desktopPagination.page = page
     },
     onUpdatePageSize: (pageSize: number) => {
-        tablePagination.pageSize = pageSize
-        tablePagination.page = 1
+        desktopPagination.pageSize = pageSize
+        desktopPagination.page = 1
     }
 })
+
+export const tablePagination = computed(() => 
+    useMobileLayout.value ? mobilePagination : desktopPagination
+)
